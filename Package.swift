79be7bcdba072be46adc37c609c9a7879c89c5a2
hamzaabujarad <hamza.ahmad.ma@gmail.com>
2024-12-02 15:06:47 +0300

    fix swift package and add missing headers

--- Package.swift
+++ Package.swift
@@ -2,13 +2,8 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
 let package = Package(
     name: "WireGuardKit",
-    platforms: [
-        .macOS(.v12),
-        .iOS(.v15)
-    ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
     ],
